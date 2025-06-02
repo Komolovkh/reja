3ad6063df1c566a115dc59b6dd323f1d1fa3e138
Komolovkh <xurshidjonpolvon04@gmail.com>
2025-06-02 13:51:06 +0900

    developed author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 kirish codelari
 app.use(express.static("public"));
@@ -14,16 +24,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 routing code
-// app.get("/hello", function (req, res) {
-//   res.send(`<h1 style="background: red">HELLO WORLD by lucas</h1>`);
-// });
 
-// app.get("/gift", function (req, res) {
-//   res.send(`<h1>siz sovgalar bolimidasiz</h1>`);
-// });
 app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
