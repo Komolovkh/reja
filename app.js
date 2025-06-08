0629350bf1089a94399b263976f51382932bf14f
Khurshidbek <xurshidjonpolvon04@gmail.com>
2025-06-08 18:13:52 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -30,8 +30,17 @@ app.set("view engine", "ejs");
 // 4 routing code
 
 app.post("/create-item", (req, res) => {
+  console.log("user entered /");
   console.log(req.body);
-  res.json({ test: "success" });
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -39,7 +48,18 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        // console.log(data);
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
